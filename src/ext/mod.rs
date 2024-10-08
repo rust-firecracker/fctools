ac16c9c204998f944757b596f2f9f0a34fd696db
kanpov <karpovanton729@gmail.com>
2024-10-08 15:33:25 +0300

    Optimize and slim down debug builds, attempt to render feature flags properly on docs.rs once published

--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,12 +1,23 @@
 #[cfg(feature = "fcnet-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "fcnet-ext")))]
 pub mod fcnet;
+
 #[cfg(feature = "grpc-vsock-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "grpc-vsock-ext")))]
 pub mod grpc_vsock;
+
 #[cfg(feature = "http-vsock-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "http-vsock-ext")))]
 pub mod http_vsock;
+
 #[cfg(feature = "link-local-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "link-local-ext")))]
 pub mod link_local;
+
 #[cfg(feature = "metrics-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "metrics-ext")))]
 pub mod metrics;
+
 #[cfg(feature = "snapshot-editor-ext")]
+#[cfg_attr(docsrs, doc(cfg(feature = "snapshot-editor-ext")))]
 pub mod snapshot_editor;
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 #[cfg(feature = "executor")]
+#[cfg_attr(docsrs, doc(cfg(feature = "executor")))]
 pub mod executor;
+
 pub mod ext;
+
 #[cfg(feature = "process")]
+#[cfg_attr(docsrs, doc(cfg(feature = "process")))]
 pub mod process;
+
 #[cfg(feature = "shell-spawner")]
+#[cfg_attr(docsrs, doc(cfg(feature = "shell-spawner")))]
 pub mod shell_spawner;
+
 #[cfg(feature = "vm")]
+#[cfg_attr(docsrs, doc(cfg(feature = "vm")))]
 pub mod vm;
