0dc6e181f51d8ab5704479dc67ea1c86026b7b7e
kanpov <karpovanton729@gmail.com>
2024-12-04 16:35:25 +0300

    Clean up feature handling of nix/rustix option, with clear preference of rustix
    Signed-off-by: kanpov <karpovanton729@gmail.com>

--- src/sys.rs
+++ src/sys.rs
@@ -1,77 +1,38 @@
-#![allow(unused)]
+#[cfg(all(feature = "sys-nix", not(feature = "sys-rustix")))]
+mod imp_nix {
+    use std::{
+        os::fd::{FromRawFd, OwnedFd, RawFd},
+        path::Path,
+    };
 
-use std::os::fd::{AsFd, BorrowedFd, OwnedFd, RawFd};
-use std::path::Path;
+    use nix::sys::stat::Mode as NixMode;
 
-#[cfg(feature = "sys-nix")]
-use nix::sys::stat::Mode as NixMode;
-#[cfg(feature = "sys-rustix")]
-use rustix::fs::Mode as RustixMode;
-#[cfg(feature = "sys-rustix")]
-use std::os::fd::FromRawFd;
-
-#[inline]
-pub fn chown(path: &Path, uid: u32, gid: u32) -> Result<(), std::io::Error> {
-    if cfg!(feature = "sys-rustix") {
-        rustix_op(rustix::fs::chown(
-            path,
-            Some(unsafe { rustix::fs::Uid::from_raw(uid) }),
-            Some(unsafe { rustix::fs::Gid::from_raw(gid) }),
-        ))
-    } else {
-        nix_op(nix::unistd::chown(path, Some(uid.into()), Some(gid.into())))
+    #[inline]
+    pub fn chown(path: &Path, uid: u32, gid: u32) -> Result<(), std::io::Error> {
+        nix::unistd::chown(path, Some(uid.into()), Some(gid.into())).map_err(|_| std::io::Error::last_os_error())
     }
-}
 
-#[inline]
-pub fn geteuid() -> u32 {
-    if cfg!(feature = "sys-rustix") {
-        rustix::process::geteuid().as_raw()
-    } else {
+    #[inline]
+    pub fn geteuid() -> u32 {
         nix::unistd::geteuid().as_raw()
     }
-}
 
-#[inline]
-pub fn getegid() -> u32 {
-    if cfg!(feature = "sys-rustix") {
-        rustix::process::getegid().as_raw()
-    } else {
+    #[inline]
+    pub fn getegid() -> u32 {
         nix::unistd::getegid().as_raw()
     }
-}
 
-#[inline]
-pub fn mkfifo(path: &Path) -> Result<(), std::io::Error> {
-    if cfg!(feature = "sys-rustix") {
-        rustix_op(rustix::fs::mknodat(
-            unsafe { BorrowedFd::borrow_raw(0) },
-            path,
-            rustix::fs::FileType::Fifo,
-            RustixMode::ROTH | RustixMode::WOTH | RustixMode::RUSR | RustixMode::WUSR,
-            u64::MAX,
-        ))
-    } else {
-        nix_op(nix::unistd::mkfifo(
+    #[inline]
+    pub fn mkfifo(path: &Path) -> Result<(), std::io::Error> {
+        nix::unistd::mkfifo(
             path,
             NixMode::S_IROTH | NixMode::S_IWOTH | NixMode::S_IRUSR | NixMode::S_IWUSR,
-        ))
+        )
+        .map_err(|_| std::io::Error::last_os_error())
     }
-}
 
-#[inline]
-pub fn pidfd_open(pid: i32) -> Result<OwnedFd, std::io::Error> {
-    if cfg!(feature = "sys-rustix") {
-        rustix_op(rustix::process::pidfd_open(
-            rustix::process::Pid::from_raw(pid).ok_or_else(|| {
-                std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "The provided PID for pidfd_open was negative",
-                )
-            })?,
-            rustix::process::PidfdFlags::empty(),
-        ))
-    } else {
+    #[inline]
+    pub fn pidfd_open(pid: i32) -> Result<OwnedFd, std::io::Error> {
         // pidfd_open isn't wrapped in nix or libc, so a libc-wrapped syscall is needed
         let fd = unsafe { nix::libc::syscall(nix::libc::SYS_pidfd_open, pid, 0) };
 
@@ -81,16 +42,9 @@ pub fn pidfd_open(pid: i32) -> Result<OwnedFd, std::io::Error> {
 
         Ok(unsafe { OwnedFd::from_raw_fd(fd as i32) })
     }
-}
 
-#[inline]
-pub fn pidfd_send_sigkill(fd: RawFd) -> Result<(), std::io::Error> {
-    if cfg!(feature = "sys-rustix") {
-        rustix_op(rustix::process::pidfd_send_signal(
-            unsafe { BorrowedFd::borrow_raw(fd) },
-            rustix::process::Signal::Kill,
-        ))
-    } else {
+    #[inline]
+    pub fn pidfd_send_sigkill(fd: RawFd) -> Result<(), std::io::Error> {
         // pidfd_send_signal isn't wrapped in nix or libc, so a libc-wrapped syscall is needed
         let ret = unsafe { nix::libc::syscall(nix::libc::SYS_pidfd_send_signal, fd, nix::libc::SIGKILL, 0, 0) };
 
@@ -102,12 +56,70 @@ pub fn pidfd_send_sigkill(fd: RawFd) -> Result<(), std::io::Error> {
     }
 }
 
-#[inline(always)]
-fn rustix_op<T>(result: Result<T, rustix::io::Errno>) -> Result<T, std::io::Error> {
-    result.map_err(|errno| std::io::Error::from_raw_os_error(errno.raw_os_error()))
+#[cfg(feature = "sys-rustix")]
+mod imp_rustix {
+    use std::{
+        os::fd::{BorrowedFd, OwnedFd, RawFd},
+        path::Path,
+    };
+
+    use rustix::fs::Mode;
+
+    #[inline]
+    pub fn chown(path: &Path, uid: u32, gid: u32) -> Result<(), std::io::Error> {
+        rustix::fs::chown(
+            path,
+            Some(unsafe { rustix::fs::Uid::from_raw(uid) }),
+            Some(unsafe { rustix::fs::Gid::from_raw(gid) }),
+        )
+        .map_err(|errno| std::io::Error::from_raw_os_error(errno.raw_os_error()))
+    }
+
+    #[inline]
+    pub fn geteuid() -> u32 {
+        rustix::process::geteuid().as_raw()
+    }
+
+    #[inline]
+    pub fn getegid() -> u32 {
+        rustix::process::getegid().as_raw()
+    }
+
+    #[inline]
+    pub fn mkfifo(path: &Path) -> Result<(), std::io::Error> {
+        rustix::fs::mknodat(
+            unsafe { BorrowedFd::borrow_raw(0) },
+            path,
+            rustix::fs::FileType::Fifo,
+            Mode::ROTH | Mode::WOTH | Mode::RUSR | Mode::WUSR,
+            u64::MAX,
+        )
+        .map_err(|errno| std::io::Error::from_raw_os_error(errno.raw_os_error()))
+    }
+
+    #[inline]
+    pub fn pidfd_open(pid: i32) -> Result<OwnedFd, std::io::Error> {
+        rustix::process::pidfd_open(
+            rustix::process::Pid::from_raw(pid).ok_or_else(|| {
+                std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "The provided PID for pidfd_open was negative",
+                )
+            })?,
+            rustix::process::PidfdFlags::empty(),
+        )
+        .map_err(|errno| std::io::Error::from_raw_os_error(errno.raw_os_error()))
+    }
+
+    #[inline]
+    pub fn pidfd_send_sigkill(fd: RawFd) -> Result<(), std::io::Error> {
+        rustix::process::pidfd_send_signal(unsafe { BorrowedFd::borrow_raw(fd) }, rustix::process::Signal::Kill)
+            .map_err(|errno| std::io::Error::from_raw_os_error(errno.raw_os_error()))
+    }
 }
 
-#[inline(always)]
-fn nix_op<T>(result: Result<T, nix::Error>) -> Result<T, std::io::Error> {
-    result.map_err(|_| std::io::Error::last_os_error())
-}
+#[cfg(feature = "sys-rustix")]
+pub use imp_rustix::*;
+
+#[cfg(all(feature = "sys-nix", not(feature = "sys-rustix")))]
+pub use imp_nix::*;
