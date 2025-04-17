de18fb5d6fedad885fc3c94a1e9e1479d98e4032
kanpov <karpovanton729@gmail.com>
2025-04-17 17:11:08 +0300

    Update documentation notice for syscall backend changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 //! `fctools` is a highly modular and exhaustive SDK for developing high-performance asynchronous production applications that
 //! leverage the capabilities of Firecracker microVMs.
 //!
+//! Binary crates using fctools will need to enable a syscall backend. Libraries should not enable any syscall backend
+//! feature, and must instead expect binary crates to do so. The fctools crate without such a backend will compile but panic at runtime
+//! with the message "No syscall backend was enabled for fctools". Binary crate developers must choose either the "nix-syscall-backend" or
+//! the "rustix-syscall-backend" feature. If the latter is enabled, the former is ignored, as only one syscall backend can exist at a time.
+//! The two features use their two respective crates: "nix" or "rustix". As such, they are functionally equivalent, with the only
+//! difference being that "nix" uses C FFI to call into libc and perform syscalls, while "rustix" invokes syscalls directly without any FFI.
+//!
 //! By default, only the [runtime] module that provides traits for supporting any asynchronous runtime is enabled. Binary
 //! crates using `fctools` should usually pull in either a built-in implementation of a [Runtime](runtime::Runtime) via either
 //! the `tokio-runtime` or `smol-runtime` features, or install a third-party crate with its own implementation.
-//!
-//! By default, the "nix" crate is used by fctools to perform syscalls. The "use-rustix" feature opts into using the "rustix"
-//! crate instead, and should typically only be used by binary crates, and not by libraries. The two internal syscall implementations
-//! are functionally equivalent, though "nix" uses C FFI to call into libc and perform syscalls, while "rustix" invokes syscalls
-//! directly without any FFI.
 //!
 //! The rest of the crate that provides actual functionality is structured in a vertical fashion, with each layer introducing more
 //! useful and high-level features than the one preceding it. There are 6 such layers, from lowest to highest level of abstraction:
